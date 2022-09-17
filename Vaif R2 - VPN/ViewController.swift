35ecab017c52423897c0095c366439d6165e8df9
vAIF LLC <vaifappios@gmail.com>
2022-09-17 14:57:50 -0400

    Initial Commit

---
+++ Vaif R2 - VPN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vaif R2 - VPN
+//
+//  Created by VAIF on 9/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
